41a5ab2096d5d6bbf5c8f86cb61eab437f6dc9ea
chapkir <chapatisi93@gmail.com>
2025-06-27 19:52:38 +0300

    add: Favorites top bar

--- app/src/main/java/com/example/chapkirnews/presentation/screens/favorites_screen/FavoritesScreen.kt
+++ app/src/main/java/com/example/chapkirnews/presentation/screens/favorites_screen/FavoritesScreen.kt
@@ -3,6 +3,8 @@ package com.example.chapkirnews.presentation.screens.favorites_screen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
@@ -11,6 +13,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,14 +23,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.chapkirnews.R
 import com.example.chapkirnews.presentation.components.ErrorBlock
 import com.example.chapkirnews.presentation.components.news_card.NewsCard
-import com.example.chapkirnews.presentation.screens.newsfeed_screen.NewsfeedViewModel
 
 @Composable
 fun FavoritesScreen(
@@ -34,50 +39,89 @@ fun FavoritesScreen(
 ) {
     val state by viewModel.uiState.collectAsState()
 
-    Box(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        when {
-            state.isLoading -> {
-                CircularProgressIndicator(
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.background)
+                .padding(top = 14.dp, bottom = 11.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(start = 12.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(
+                    onClick = { },
                     modifier = Modifier
-                        .align(Alignment.Center)
-                        .size(50.dp),
-                    color = MaterialTheme.colorScheme.onBackground,
-                    strokeWidth = 7.dp,
-                )
-            }
-
-            state.error != null -> {
-                ErrorBlock(
-                    message = state.error!!,
-                    icon = R.drawable.ic_cross_small,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .imePadding()
-                        .align(Alignment.Center)
-                        .padding(horizontal = 25.dp)
-                )
-            }
-
-            else -> {
-                LazyColumn(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color.Black),
-                    verticalArrangement = Arrangement.spacedBy(12.dp)
+                        .padding(end = 16.dp)
+                        .size(28.dp)
                 ) {
-                    items(state.favorites) { article ->
-                        NewsCard(
-                            title = article.title,
-                            imageUrl = article.imageUrl,
-                            author = article.author,
-                            description = article.description,
-                            publishedAt = article.publishedAt,
-                            content = article.content,
-                            isFavorite = article.isFavorite,
-                            onToggleFavorite = { viewModel.toggleFavorite(article) }
-                        )
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_menu),
+                        contentDescription = "menu",
+                        modifier = Modifier.size(22.dp),
+                        tint = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+
+                Text(
+                    text = "Избранное",
+                    fontSize = 21.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    textAlign = TextAlign.Center,
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            when {
+                state.isLoading -> {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .size(50.dp),
+                        color = MaterialTheme.colorScheme.onBackground,
+                        strokeWidth = 7.dp,
+                    )
+                }
+
+                state.error != null -> {
+                    ErrorBlock(
+                        message = state.error!!,
+                        icon = R.drawable.ic_cross_small,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .imePadding()
+                            .align(Alignment.Center)
+                            .padding(horizontal = 25.dp)
+                    )
+                }
+
+                else -> {
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.Black),
+                        verticalArrangement = Arrangement.spacedBy(12.dp)
+                    ) {
+                        items(state.favorites) { article ->
+                            NewsCard(
+                                title = article.title,
+                                imageUrl = article.imageUrl,
+                                author = article.author,
+                                description = article.description,
+                                publishedAt = article.publishedAt,
+                                content = article.content,
+                                isFavorite = article.isFavorite,
+                                onToggleFavorite = { viewModel.toggleFavorite(article) }
+                            )
+                        }
                     }
                 }
             }
